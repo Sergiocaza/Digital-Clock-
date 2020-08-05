1f8adfaf629669cbedd2149f7d9624fd270f274c
Sergio Cazaresmuniz <cazaresmuniz.sergio@gmail.com>
2020-08-05 11:42:48 -0500

    setting if function

--- clock.js
+++ clock.js
@@ -14,4 +14,10 @@ function showTime(){
         session = "PM";
     }
 
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+
 }
